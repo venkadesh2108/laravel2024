cc0800969fdcc2d3e47903e9b79f153b64492ec8
venkadesh2108 <ragulvenkadesh199@email.com>
2024-02-16 10:26:50 +0530

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+// use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
+        // if (env('APP_ENV') == 'production') {
+        //     $url->forceScheme('https');
+        // }
     }
 }
